b73eada7d7747a8161d9cc2fc86cbf6c148b23e8
dv-bt <d.bottone@pm.me>
2025-08-19 14:27:03 +0200

    Updated stitching script with better support for paths

--- scripts/stitch_maps.py
+++ scripts/stitch_maps.py
@@ -21,6 +21,7 @@ def parse_arguments() -> argparse.Namespace:
         "-s",
         "--source",
         required=True,
+        type=Path,
         help="Tilset directory irectory containing images to stitch",
     )
 
@@ -28,6 +29,7 @@ def parse_arguments() -> argparse.Namespace:
         "-d",
         "--dest",
         required=True,
+        type=Path,
         help="Destination file to save the stitched image. Must be a .tif image",
     )
 
@@ -42,16 +44,32 @@ def parse_arguments() -> argparse.Namespace:
         "-v", "--verbose", action="store_true", help="Enable verbose output"
     )
 
-    return parser.parse_args()
+    parser.add_argument(
+        "-w",
+        "--working_dir",
+        required=False,
+        type=Path,
+        default=Path.cwd(),
+        help=(
+            "Optional working directory used for parsing source and destinatione path "
+            "if they are relative."
+        ),
+    )
+
+    args = parser.parse_args()
+
+    # Update paths
+    args.source = (args.working_dir / args.source).resolve()
+    args.dest = (args.working_dir / args.dest).resolve()
+
+    return args
 
 
 def main():
     """Main function to run the script."""
     args = parse_arguments()
-    tileset_path = Path(args.source)
-    dest_path = Path(args.dest)
-    dest_path.parent.mkdir(exist_ok=True)
-    stitch_images(tileset_path, dest_path, args.compress, args.verbose)
+    args.dest.parent.mkdir(exist_ok=True)
+    stitch_images(args.source, args.dest, args.compress, args.verbose)
 
 
 if __name__ == "__main__":
